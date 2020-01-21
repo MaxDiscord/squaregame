d814d8e8fea7879ce59e1450dad036353855d7bf
MMaxM33 <39847299+MaxDiscord@users.noreply.github.com>
2020-01-20 19:29:23 -0700

    Assets Folder

---
+++ Assets/Playermove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermove : MonoBehaviour
+{
+    public float forwardSpeed;
+    public float sideSpeed;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Variables Defined");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(-forwardSpeed * Time.deltaTime, 0, 0);
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(0,0,sideSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(0, 0, -sideSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/touch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class touch : MonoBehaviour
+{
+    public Transform caps;
+    public Rigidbody playerBody;
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name != "Plane") 
+        {
+            if (collision.gameObject.name == "Plane (1)")
+            {
+                Debug.Log("You win!");
+                SceneManager.LoadScene("Level02");
+            }
+            else
+            {
+                transform.position = caps.position;
+
+            }
+
+            Debug.Log("Game over");
+        }
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
